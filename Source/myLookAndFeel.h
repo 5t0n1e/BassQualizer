1187cbb6231a79f3249d734064d7ee73f3ef55b2
5t0n1e <cisverbruggen@hotmail.com>
2024-10-21 11:56:47 +0200

    Custom buttons (with files)

---
+++ Source/myLookAndFeel.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <JuceHeader.h>
+
+//==============================================================================
+class myLookAndFeelV1 : public juce::LookAndFeel_V4
+{
+public:
+    myLookAndFeelV1();
+
+    void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos,
+                          float rotaryStartAngle, float rotaryEndAngle, juce::Slider& slider) override;
+
+private:
+    juce::Image img1;
+
+};
+
+//==============================================================================
+class myLookAndFeelV3 : public juce::LookAndFeel_V4
+{
+
+public:
+    myLookAndFeelV3();
+
+    void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos,
+                          float rotaryStartAngle, float rotaryEndAngle, juce::Slider& slider) override;
+
+private:
+    juce::Image img2;
+};
